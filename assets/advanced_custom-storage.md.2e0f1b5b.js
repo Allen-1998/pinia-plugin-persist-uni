61bbc269d924bccde43f403dcf33cecd61fe2aa3
Allen-1998 <73213399+Allen-1998@users.noreply.github.com>
2022-04-23 20:49:49 +0000

    Deploying to gh-pages from @ Allen-1998/pinia-plugin-persist-uni@78f9259e0b50efc682b5846416fee163bb178325 🚀

--- assets/advanced_custom-storage.md.2e0f1b5b.js
+++ assets/advanced_custom-storage.md.2e0f1b5b.js
@@ -1,4 +1,4 @@
-import{_ as s,c as n,o as a,a as t}from"./app.94291136.js";const _='{"title":"Custom storage","description":"","frontmatter":{},"headers":[],"relativePath":"advanced/custom-storage.md"}',o={},e=t(`<h1 id="custom-storage" tabindex="-1">Custom storage <a class="header-anchor" href="#custom-storage" aria-hidden="true">#</a></h1><p>By default the storage is set to localStorage, but you can specify the storage you want to use for each strategy by setting the <code>storage</code> key.</p><p>You can then use <code>sessionStorage</code>or <code>localStorage</code>.</p><p>priority: strategies/storage &gt; H5Storage &gt; defaultStorage(localStorage)</p><div class="language-typescript"><pre><code><span class="token comment">// store/use-user-store.ts</span>
+import{_ as s,c as n,o as a,a as t}from"./app.94291136.js";const _='{"title":"Custom storage","description":"","frontmatter":{},"headers":[],"relativePath":"advanced/custom-storage.md"}',o={},e=t(`<h1 id="custom-storage" tabindex="-1">Custom storage <a class="header-anchor" href="#custom-storage" aria-hidden="true">#</a></h1><p>By default the storage is set to sessionStorage, but you can specify the storage you want to use for each strategy by setting the <code>storage</code> key.</p><p>You can then use <code>sessionStorage</code>or <code>localStorage</code>.</p><p>priority: strategies/storage &gt; H5Storage &gt; defaultStorage(sessionStorage)</p><div class="language-typescript"><pre><code><span class="token comment">// store/use-user-store.ts</span>
 
 <span class="token keyword">export</span> <span class="token keyword">const</span> useUserStore <span class="token operator">=</span> <span class="token function">defineStore</span><span class="token punctuation">(</span><span class="token string">&#39;storeUser&#39;</span><span class="token punctuation">,</span> <span class="token punctuation">{</span>
   <span class="token function">state</span><span class="token punctuation">(</span><span class="token punctuation">)</span> <span class="token punctuation">{</span>
@@ -10,10 +10,10 @@ import{_ as s,c as n,o as a,a as t}from"./app.94291136.js";const _='{"title":"Cu
   <span class="token punctuation">}</span><span class="token punctuation">,</span>
   persist<span class="token operator">:</span> <span class="token punctuation">{</span>
     enabled<span class="token operator">:</span> <span class="token boolean">true</span><span class="token punctuation">,</span>
-    H5Storage<span class="token operator">:</span> sessionStorage<span class="token punctuation">,</span>
+    H5Storage<span class="token operator">:</span> localStorage<span class="token punctuation">,</span>
     strategies<span class="token operator">:</span> <span class="token punctuation">[</span>
       <span class="token punctuation">{</span>
-        storage<span class="token operator">:</span> localStorage<span class="token punctuation">,</span>
+        storage<span class="token operator">:</span> sessionStorage<span class="token punctuation">,</span>
         paths<span class="token operator">:</span> <span class="token punctuation">[</span><span class="token string">&#39;accessToken&#39;</span><span class="token punctuation">]</span><span class="token punctuation">,</span>
       <span class="token punctuation">}</span><span class="token punctuation">,</span>
     <span class="token punctuation">]</span><span class="token punctuation">,</span>
