9f2ececfb9a9ed4d00a235c238eb597bb2d4905f
Allen-1998 <73213399+Allen-1998@users.noreply.github.com>
2022-04-23 21:29:26 +0000

    Deploying to gh-pages from @ Allen-1998/pinia-plugin-persist-uni@b618078578a44161fcacb4711188b377ea09e2e3 🚀

--- assets/advanced_custom-storage.md.2e0f1b5b.lean.js
+++
@@ -1,1 +0,0 @@
-import{_ as s,c as n,o as a,a as t}from"./app.94291136.js";const _='{"title":"Custom storage","description":"","frontmatter":{},"headers":[],"relativePath":"advanced/custom-storage.md"}',o={},e=t("",5),p=[e];function c(r,u,l,i,k,d){return a(),n("div",null,p)}var x=s(o,[["render",c]]);export{_ as __pageData,x as default};
---
+++ assets/advanced_custom-storage.md.a829aff2.lean.js
@@ -0,0 +1,1 @@
+import{_ as s,c as n,o as a,a as t}from"./app.94291136.js";const _='{"title":"Custom storage","description":"","frontmatter":{},"headers":[],"relativePath":"advanced/custom-storage.md"}',o={},e=t("",6),p=[e];function c(r,u,l,i,k,d){return a(),n("div",null,p)}var h=s(o,[["render",c]]);export{_ as __pageData,h as default};
